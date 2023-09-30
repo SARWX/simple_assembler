2d785076ec3a23adbf67a01d104eb255b801b8da
Timofei Novikov <tictacnumt@gmail.com>
2023-09-30 14:42:36 +0300

    new method with bit-banding

--- example.s
+++ example.s
@@ -13,22 +13,23 @@ main:
     LDR R2, =0x44444424     @       New state of GPIOC_CRH
     STR R2, [R0]            @       Write new state to the GPIOC_CRH    *PC9 = output push-pull max speed 2 MHz
 
-    LDR R0, =0x40011010     @       Adress of GPIOC_BSRR = = 0x40011010, simbol = idicates that the command is pseudo
-    LDR R2, =0x200          @       New state of GPIOC_BSRR SET led
+    LDR R0, =0x42220264     @       Adress of 25th bit GPIOC_BSRR = 0x42220264 BIT RESET, simbol = idicates that the command is pseudo
+    LDR R1, =0x42220224     @       Adress of 9th bit GPIOC_BSRR = 0x42220264 BIT SET, simbol = idicates that the command is pseudo
+    MOV R2, #0x1            
+    STR R2, [R1]            @       Initial set state
 
 toggle:
-    STR R2, [R0]            @       Toggle led
-    CMP R2, #0x200          @       Check if led is set
-    ITE NE
-    LDRNE R2, =0x200        @       Set if wasn't set
-    LDREQ R2, =0x2000000    @       Reset if was set
 
-    LDR R1, =0xFFFFF        @       Set counter 
+    STR R2, [R0]            @       Toggle reset state
+    EOR R2, R2, #0x1        @       Change led state
+    STR R2, [R1]            @       Toggle set state
+
+    LDR R3, =0xFFFFF        @       Set counter 
     B wait                  @       Execute wait function
 
 wait:
-    CMP R1, #0x0            @       While counter R0 > 0
+    CMP R3, #0x0            @       While counter R0 > 0
     BEQ toggle              @       If R0 == 0 toggle led
-    SUB R1, #0x1            @       Decrement counter
+    SUB R3, #0x1            @       Decrement counter
     B wait                  @       Return to the wait
     
